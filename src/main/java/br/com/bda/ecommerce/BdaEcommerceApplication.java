80822072658566f1375abd55b54c9dc4d757cb41
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2021-09-27 19:51:54 -0300

    Feat: Configurações iniciais
    Configurações iniciais do ambiente e do Banco de Dados.

---
+++ src/main/java/br/com/bda/ecommerce/BdaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.bda.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdaEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdaEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/bda/ecommerce/BdaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bda.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdaEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
