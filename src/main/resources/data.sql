f462e4396d5e29ff78a3ed1377d9f573d85b429f
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2021-09-28 23:28:20 -0300

    Feat: Criação do Banco de Dados e Scripts
    Criado o Banco de Dados e os Scripts DDL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, dataUltimaAtualizacao, preco)
+VALUES (1, 'Tabuleiro Star Wars', 'Tabuleiro personalizado de Star Wars para o jogo Brincando de Aprender', 'https://photos.enjoei.com.br/xadrez-star-wars/1200xN/czM6Ly9waG90b3MuZW5qb2VpLmNvbS5ici9wcm9kdWN0cy81MjUzMjY0L2MyOWJmZjNiYzBlYzVlZGU1NmI2MjA3MGQ3NDA0Y2E4LmpwZw', '2021-03-28', CURRENT_TIMESTAMP(), '299.00');
+
+INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, dataUltimaAtualizacao, preco)
+VALUES (2, 'Tabuleiro Toy Story', 'Tabuleiro personalizado de Toy Story para o jogo Brincando de Aprender', 'https://img.elo7.com.br/product/zoom/28DC298/jogo-de-tabuleiro-toy-story-tabuleiro.jpg', '2021-05-12', CURRENT_TIMESTAMP(), '400.00');
+
+INSERT INTO produto(id, nome, descricao, fotoUrl, dataCadastro, dataUltimaAtualizacao, preco)
+VALUES (3, 'Tabuleiro Carros', 'Tabuleiro personalizado do filme Carros para o jogo Brincando de Aprender', 'https://img.elo7.com.br/product/original/27BE7BE/jogo-de-tabuleiro-carros-de-corrida-personalizado.jpg', '2021-08-16', CURRENT_TIMESTAMP(), '550.00');
